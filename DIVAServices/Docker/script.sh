c2c58a1a78774617da5b621bd5ffaeabefacb993
Marco vR <marcovr@live.de>
2018-06-20 14:05:08 +0200

    cleanup

--- DIVAServices/Docker/script.sh
+++ DIVAServices/Docker/script.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-inputFile=$1
-r=$2
-g=$3
-outputFile=${4}out.txt
+inputFile="$1"
+r="$2"
+g="$3"
+outputFile="${4}out.txt"
 
 cd /input/
 
 mkdir data
-if [ ${inputFile: -4} == ".zip" ]; then
-	unzip -d data $inputFile
+# unzip, if necessary
+if [ "${inputFile: -4}" == ".zip" ]; then
+	unzip -d data "$inputFile"
 else
-	cp $inputFile data
+	cp "$inputFile" data
 fi
 
-python run.py -r $r -g $g data > $outputFile
+python run.py -r "$r" -g "$g" data > "$outputFile"
