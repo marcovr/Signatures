51f77c4ec2d50c1940da031e139300d77fd08968
Marco vR <marcovr@live.de>
2018-06-20 14:04:16 +0200

    added verification mode option to docker image

--- DIVAServices/Docker/script.sh
+++ DIVAServices/Docker/script.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-inputFile=$1
-n=$2
-outputFile=${3}out.txt
+inputFile="$1"
+n="$2"
+v="$3"
+outputFile="${4}out.txt"
+
+# v=0 -> don't apply -v option
+# v=1 -> apply -v option
+if [ "$v" == "0" ]; then
+	v=""
+else
+	v="-v"
+fi
 
 cd /input/
 
 mkdir data
-unzip -d data $inputFile
+unzip -d data "$inputFile"
 
-java -jar gedwrapper.jar -fr $n -o $outputFile data
+java -jar gedwrapper.jar "$v" -fr "$n" -o "$outputFile" data
