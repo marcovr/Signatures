ada2b7b8e3b4683d2a6c1774d688a914eeb1b545
Marco vR <marcovr@live.de>
2018-06-20 14:01:08 +0200

    added batch processing with zips

--- DIVAServices/Docker/script.sh
+++ DIVAServices/Docker/script.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
-inputFile=$1
-outputFile=${2}imageGraph.xml
+inputFile="$1"
+outputFolder="$2"
+outputFile="${2}imageGraph"
 
 cd /input/
 
-python3 run.py -i $inputFile -o $outputFile
+mkdir data
+mkdir out
+# unzip, if necessary
+if [ "${inputFile: -4}" == ".zip" ]; then
+	unzip -d data "$inputFile"
+else
+	cp "$inputFile" data
+fi
+
+for inFile in data/*; do
+	base=$(basename "$inFile")
+	outFile="out/${base%.*}.xml"
+	
+	python3 run.py -i "$inFile" -o "$outFile"
+done
+
+# rezip, if necessary
+if [ "${inputFile: -4}" == ".zip" ]; then
+	zip -j "${outputFile}.zip" out/*
+else
+	mv out/* "${outputFile}.xml"
+fi
