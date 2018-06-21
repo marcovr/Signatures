e3e9acf74ecf67041fbc9327aea8ce1d4add7fef
Marco vR <marcovr@live.de>
2018-06-21 15:37:01 +0200

    updated docker files

--- DIVAServices/Docker/script.sh
+++ DIVAServices/Docker/script.sh
@@ -2,7 +2,8 @@
 inputFile="$1"
 r="$2"
 g="$3"
-outputFile="${4}out.txt"
+v="$4"
+outputFile="${5}results.json"
 
 cd /input/
 
@@ -10,8 +11,22 @@ mkdir data
 # unzip, if necessary
 if [ "${inputFile: -4}" == ".zip" ]; then
 	unzip -d data "$inputFile"
+
+	# v=0 -> don't apply -v option
+	# v=1 -> apply -v option
+	if [ "$v" != "0" ]; then
+		mkdir verify
+		files=(data/*)
+		file="${files[-1]}"
+		v_name=$(basename "$file")
+		v_file="verify/$v_name"
+		mv "$file" "$v_file"
+
+		python run.py -r "$r" -g "$g" -s -j "$outputFile" -v "$v_file" data
+		exit
+	fi
 else
 	cp "$inputFile" data
 fi
 
-python run.py -r "$r" -g "$g" data > "$outputFile"
+python run.py -r "$r" -g "$g" -s -j "$outputFile" data
