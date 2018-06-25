7e5eec153d6d9b2a56e8b8dd24ffeed145b9ba3a
Marco vR <marcovr@live.de>
2018-06-25 15:51:52 +0200

    POSIX compliance

--- DIVAServices/Docker/script.sh
+++ DIVAServices/Docker/script.sh
@@ -5,22 +5,21 @@ g="$3"
 v="$4"
 outputFile="${5}results.json"
 
-cd /input/
+cd /input/ || exit
 
 mkdir data
 # unzip, if necessary
-if [ "${inputFile: -4}" == ".zip" ]; then
+if [ "${inputFile##*.}" = "zip" ]; then
 	unzip -d data "$inputFile"
 
 	# v=0 -> don't apply -v option
 	# v=1 -> apply -v option
 	if [ "$v" != "0" ]; then
 		mkdir verify
-		files=(data/*)
-		file="${files[-1]}"
-		v_name=$(basename "$file")
+		for last_file in data/*; do true; done
+		v_name=$(basename "$last_file")
 		v_file="verify/$v_name"
-		mv "$file" "$v_file"
+		mv "$last_file" "$v_file"
 
 		python run.py -r "$r" -g "$g" -s -j "$outputFile" -v "$v_file" data
 		exit
